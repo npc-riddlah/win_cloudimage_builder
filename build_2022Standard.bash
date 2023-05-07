88251ff954d0277f557f43321e7c8ce777c25a5d
riddlah <riddlah@yandex.ru>
2023-05-07 12:53:27 +0300

    Little fix

--- build_2022Standard.bash
+++ build_2022Standard.bash
@@ -4,7 +4,8 @@ PATH_MNT=/mnt/win
 PATH_ISO=./ISO/win22ru.iso
 PATH_PE=./ISO/WinPE_amd64.iso
 PATH_ELEMENT=./elements/win2022_ru_base/
+PATH_UNATTENDXML=./elements/win2022_ru_base/Unattend.xml
 VAR_NAME=Windows\ Server\ 2022\ SERVERSTANDARD
 VAR_SIZE=20G
 
-./scripts/createImage.bash -i $PATH_IMG -m $PATH_MNT -s $VAR_SIZE -I $PATH_ISO -w $PATH_PE -n "$VAR_NAME" -e $PATH_ELEMENT
+./scripts/createImage.bash -i $PATH_IMG -m $PATH_MNT -s $VAR_SIZE -I $PATH_ISO -w $PATH_PE -n "$VAR_NAME" -e $PATH_ELEMENT -u $PATH_UNATTENDXML
