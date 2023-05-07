b2c1069886eb4289eebde15b43f1708b4044aca7
riddlah <riddlah@yandex.ru>
2023-05-07 22:33:31 +0300

    Added mainhook

---
+++ scripts/resources/build/mainhook.cmd
@@ -0,0 +1,9 @@
+cd "C:\hooks\install\"
+for /r %%v in (*.cmd) do start /wait %%v
+cd "C:\hooks\configure\"
+for /r %%v in (*.cmd) do start /wait %%v
+cd "C:\hooks\clean\"
+for /r %%v in (*.cmd) do start /wait %%v
+del "C:\hooks\*" /f /q /s
+rmdir "C:\hooks\" /s /q 
+del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\mainhook.cmd" /f /q /s
