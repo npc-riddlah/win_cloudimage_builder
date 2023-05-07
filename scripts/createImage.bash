1535ffa197536ed7692a3bb869fceb0d12355edf
riddlah <riddlah@yandex.ru>
2023-05-07 13:26:32 +0300

    Added root check

--- scripts/createImage.bash
+++ scripts/createImage.bash
@@ -99,6 +99,11 @@ run_win(){
 	qemu-system-x86_64 -accel kvm -m 2048 -boot c -hda $1 -vga virtio -spice port=5900,addr=0.0.0.0,disable-ticketing=on -bios /usr/share/qemu/OVMF.fd -cpu host -net nic -net tap,script=no,downscript=no
 }
 
+if [ "$EUID" -ne 0 ]
+  then warn_out "Please run as root (sudo)"
+  exit
+fi
+
 #Parsing commandline here
 POSITIONAL_ARGS=()
 (( ELEMENT_COUNT=0 ))
