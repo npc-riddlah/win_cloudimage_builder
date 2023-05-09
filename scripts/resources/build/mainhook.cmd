ff8be9679000f9203ac02b45723ef58724f66ce4
riddlah <riddlah@yandex.ru>
2023-05-09 23:15:21 +0300

    Now with autologon

--- scripts/resources/build/mainhook.cmd
+++ scripts/resources/build/mainhook.cmd
@@ -1,9 +1,27 @@
+chcp 65001
+
+wmic useraccount where name="Администратор" rename Administrator
+
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d Administrator /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d password /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v ForceAutoLogon /t REG_SZ /d 1 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultDomainName /t REG_SZ /d /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v IgnoreShiftOvveride /t REG_SZ /d 1 /f
+
 cd "C:\hooks\install\"
 for /r %%v in (*.cmd) do start /wait %%v
 cd "C:\hooks\configure\"
 for /r %%v in (*.cmd) do start /wait %%v
 cd "C:\hooks\clean\"
 for /r %%v in (*.cmd) do start /wait %%v
+
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v ForceAutoLogon /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultDomainName /f
+
 del "C:\hooks\*" /f /q /s
 shutdown /s /t 10
 rmdir "C:\hooks\" /s /q 
