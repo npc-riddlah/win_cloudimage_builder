d36b301dc5cb082cce349399a19f4874bb5e1c5a
Vladimir Kruss <riddlah@yandex.ru>
2023-05-09 16:31:33 +0000

    Fixed network

--- scripts/runLegacy.bash
+++ scripts/runLegacy.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 #$1 - Path to raw image
-qemu-system-x86_64 -accel kvm -m 2048 -hda $1 -vga virtio -spice port=5900,addr=0.0.0.0,disable-ticketing=on -cpu host -net nic -net tap,script=no,downscript=no
+qemu-system-x86_64 -accel kvm -m 2048 -hda $1 -vga virtio -spice port=5900,addr=0.0.0.0,disable-ticketing=on
