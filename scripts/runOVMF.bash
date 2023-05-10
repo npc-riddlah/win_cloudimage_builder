e302abf036a273d28d526f405aa1fdd74f21c87c
riddlah <riddlah@yandex.ru>
2023-05-10 12:55:27 +0300

    Now with selected CPU

--- scripts/runOVMF.bash
+++ scripts/runOVMF.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 #$1 - Path to raw image
-qemu-system-x86_64 -accel kvm -m 2048 -boot c -hda $1 -vga virtio -spice port=5900,addr=0.0.0.0,disable-ticketing=on -bios /usr/share/qemu/OVMF.fd
+qemu-system-x86_64 -accel kvm -m 2048 -boot c -hda $1 -vga virtio -spice port=5900,addr=0.0.0.0,disable-ticketing=on -bios /usr/share/qemu/OVMF.fd -smp cores=4 -cpu Skylake-Client-v3
