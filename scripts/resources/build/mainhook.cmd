27f4c3f9b054d9e57423b316ad0e7e27bf2104fa
Vladimir Kruss <riddlah@yandex.ru>
2023-05-10 12:06:02 +0000

    Autologon fix

--- scripts/resources/build/mainhook.cmd
+++ scripts/resources/build/mainhook.cmd
@@ -1,14 +1,5 @@
 chcp 65001
 
-wmic useraccount where name="Администратор" rename Administrator
-
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d Administrator /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d password /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v ForceAutoLogon /t REG_SZ /d 1 /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultDomainName /t REG_SZ /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v IgnoreShiftOvveride /t REG_SZ /d 1 /f
-
 cd "C:\hooks\install\"
 for /r %%v in (*.cmd) do start /wait %%v
 cd "C:\hooks\configure\"
