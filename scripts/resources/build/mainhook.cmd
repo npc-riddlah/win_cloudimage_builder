512c702cb7c282b2cfd30c9499a15db1054f4669
Vladimir Kruss <riddlah@yandex.ru>
2023-06-10 10:11:00 +0000

    Encoding fix

--- scripts/resources/build/mainhook.cmd
+++ scripts/resources/build/mainhook.cmd
@@ -1,16 +1,16 @@
 chcp 65001
 cd "C:\hooks\preinstall\"
-for /r %%v in (*.cmd) do start /wait /b %%v ^> COM1
-for /r %%v in (*.ps1) do powershell "& ""%%v""" > COM1
+for /r %%v in (*.ps1) do echo %%v > COM1 & powershell "& ""%%v""" -Encoding "utf8BOM" > COM1
+for /r %%v in (*.cmd) do echo %%v > COM1 & start /wait /b %%v ^> COM1
 cd "C:\hooks\install\"
-for /r %%v in (*.cmd) do start /wait /b %%v ^> COM1
-for /r %%v in (*.ps1) do powershell "& ""%%v""" > COM1
+for /r %%v in (*.ps1) do echo %%v > COM1 & powershell "& ""%%v""" -Encoding "utf8BOM" > COM1
+for /r %%v in (*.cmd) do echo %%v > COM1 & start /wait /b %%v ^> COM1
 cd "C:\hooks\configure\"
-for /r %%v in (*.cmd) do start /wait /b %%v ^> COM1
-for /r %%v in (*.ps1) do powershell "& ""%%v""" > COM1
+for /r %%v in (*.ps1) do echo %%v > COM1 & powershell "& ""%%v""" -Encoding "utf8BOM" > COM1
+for /r %%v in (*.cmd) do echo %%v > COM1 & start /wait /b %%v ^> COM1
 cd "C:\hooks\clean\"
-for /r %%v in (*.cmd) do start /wait /b %%v ^> COM1
-for /r %%v in (*.ps1) do powershell "& ""%%v""" > COM1
+for /r %%v in (*.ps1) do echo %%v > COM1 & powershell "& ""%%v""" -Encoding "utf8BOM" > COM1
+for /r %%v in (*.cmd) do echo %%v > COM1 & start /wait /b %%v ^> COM1
 
 reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /f
 reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /f
