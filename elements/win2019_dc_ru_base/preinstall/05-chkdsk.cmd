4d2fdae606342f4c0f7d595fc4d20f3ab8d43ecf
Vladimir Kruss <riddlah@yandex.ru>
2023-06-10 17:38:46 +0000

    Fixes with exit

--- elements/win2019_dc_ru_base/preinstall/05-chkdsk.cmd
+++ elements/win2019_dc_ru_base/preinstall/05-chkdsk.cmd
@@ -1,2 +1,3 @@
 echo y | chkdsk C: /F
 del /f /q /a  C:\hooks\preinstall\05-chkdsk.cmd & exit
+exit
--- elements/win2022_dc_ru_base/preinstall/01-bootsect-config.cmd
+++ elements/win2022_dc_ru_base/preinstall/01-bootsect-config.cmd
@@ -1,2 +1,3 @@
 bcdedit /timeout 1
 del C:\hooks\preinstall\01-bootsect-config.cmd /f /q & exit
+exit
