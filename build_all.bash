016fc4689547344ab1494793dea6ed7873f48daf
Vladimir Kruss <riddlah@yandex.ru>
2023-07-24 19:31:54 +0000

    Just running all scripts one-by-one

---
+++ build_all.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+./runbuild/build_2019DatacenterEn_GRID.bash
+./runbuild/build_2019DatacenterEn_Gameready.bash
+./runbuild/build_2019DatacenterRu_GRID.bash
+./runbuild/build_2019DatacenterRu_Gameready.bash
+./runbuild/build_2019StandardEn_GRID.bash
+./runbuild/build_2019StandardEn_Gameready.bash
+./runbuild/build_2019StandardRu_GRID.bash
+./runbuild/build_2019StandardRu_Gameready.bash
+./runbuild/build_2022DatacenterRu_GRID.bash
+./runbuild/build_2022DatacenterRu_Gameready.bash
+./runbuild/build_2022StandardRu_GRID.bash
+./runbuild/build_2022StandardRu_Gameready.bash
+exit 0
