a225866e55c97614fd44efd1810cf7cd500ffa13
npc_riddlah <riddlah@yandex.ru>
2023-07-29 13:22:37 +0300

    Fix of encoding

---
+++ elements/win2019_st_en_base/preinstall/06-enable-rdp.ps1
@@ -0,0 +1,7 @@
+#$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+#Set-PSDebug -Trace 1
+#Write-Output $OutputEncoding
+#netsh advfirewall firewall set rule group="Дистанционное управление рабочим столом" new enable=yes
+Set-NetFirewallRule -DisplayGroup "Дистанционное управление рабочим столом" -Enabled True
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+exit
