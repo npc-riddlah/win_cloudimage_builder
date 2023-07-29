e0a8aaaedf11fbfe4195ce9d786e83c5d8dc5c95
npc_riddlah <riddlah@yandex.ru>
2023-07-29 13:42:35 +0300

    Correct size

--- runbuild/build_2022StandardRu_GRID.bash
+++ runbuild/build_2022StandardRu_GRID.bash
@@ -8,7 +8,7 @@ PATH_ELEMENT=./elements/win2022_st_ru_base/		 #Path to first element. You can ad
 PATH_UNATTENDXML=./elements/win2022_st_ru_base/Unattend.xml #Path to Unattend.xml file to skip OOBE and set the first configuration on boot
 PATH_RUNNER=./scripts/runOVMF.bash			 #Path to runner. Runner - script, that will run final VM to prepare drivers, updates and elements.
 VAR_NAME=Windows\ Server\ 2022\ SERVERSTANDARD		 #Name of Windows edition in WIM file
-VAR_SIZE=21G						 #Final size of the image.
+VAR_SIZE=20G						 #Final size of the image.
 
 ./scripts/createImage.bash -i $PATH_IMG -m $PATH_MNT -s $VAR_SIZE -I $PATH_ISO -w $PATH_PE -n "$VAR_NAME" -e $PATH_ELEMENT -u $PATH_UNATTENDXML -r $PATH_RUNNER -e ./elements/cloudbase-init/ -p $PORT_SPICE -e ./elements/grid -e ./elements/virtio -e ./elements/baremetaldrv
 exit 0
