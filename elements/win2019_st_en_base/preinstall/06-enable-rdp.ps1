2735e20e91d37b327ac279a263e80264b5421b88
npc_riddlah <riddlah@yandex.ru>
2023-08-01 13:14:26 +0300

    Encoding fix

--- elements/win2019_st_en_base/preinstall/06-enable-rdp.ps1
+++ elements/win2019_st_en_base/preinstall/06-enable-rdp.ps1
@@ -2,6 +2,6 @@
 #Set-PSDebug -Trace 1
 #Write-Output $OutputEncoding
 #netsh advfirewall firewall set rule group="Дистанционное управление рабочим столом" new enable=yes
-Set-NetFirewallRule -DisplayGroup "Дистанционное управление рабочим столом" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Remote Desktop" -Enabled True
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 exit
