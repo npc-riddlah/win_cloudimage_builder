10c93c0b78fd8ecd672b87e3bb123a8108248b3c
npc_riddlah <riddlah@yandex.ru>
2023-08-01 13:30:04 +0300

    IEHarden disabling fix

--- elements/win2019_st_ru_base/preinstall/03-disable-ieharden.ps1
+++ elements/win2019_st_ru_base/preinstall/03-disable-ieharden.ps1
@@ -1,5 +1,6 @@
 reg add 'HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings' /v ZoneMap /t REG_DWORD /d 0 /f
 reg add 'HKLM\Software\Microsoft\Windows\CurrentVersion\Internet Settings' /v ZoneMap /t REG_DWORD /d 0 /f
 reg add 'HKCU\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Internet Settings' /v ZoneMap /t REG_DWORD /d 0 /f
+rundll32.exe setupapi.dll,InstallHinfSection IESoftenAdmin 128 %windir%\inf\IEHARDEN.INF
 Remove-Item C:\hooks\preinstall\03-disable-ieharden.ps1 -Force
 exit
