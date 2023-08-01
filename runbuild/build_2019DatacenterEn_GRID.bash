839c6a5ee35a169a42ec58054a1c48eb357a869f
npc_riddlah <riddlah@yandex.ru>
2023-08-01 17:50:31 +0300

    New size

--- runbuild/build_2019DatacenterEn_GRID.bash
+++ runbuild/build_2019DatacenterEn_GRID.bash
@@ -8,7 +8,7 @@ PATH_ELEMENT=./elements/win2019_dc_en_base/		 #Path to first element. You can ad
 PATH_UNATTENDXML=./elements/win2019_dc_en_base/Unattend.xml #Path to Unattend.xml file to skip OOBE and set the first configuration on boot
 PATH_RUNNER=./scripts/runOVMF.bash			 #Path to runner. Runner - script, that will run final VM to prepare drivers, updates and elements.
 VAR_NAME=Windows\ Server\ 2019\ SERVERDATACENTER		 #Name of Windows edition in WIM file
-VAR_SIZE=20G						 #Final size of the image.
+VAR_SIZE=21G						 #Final size of the image.
 
 ./scripts/createImage.bash -i $PATH_IMG -m $PATH_MNT -s $VAR_SIZE -I $PATH_ISO -w $PATH_PE -n "$VAR_NAME" -e $PATH_ELEMENT -u $PATH_UNATTENDXML -r $PATH_RUNNER -e ./elements/cloudbase-init/ -p $PORT_SPICE -e ./elements/grid -e ./elements/virtio -e ./elements/baremetaldrv -e ./elements/chrome
 exit 0
