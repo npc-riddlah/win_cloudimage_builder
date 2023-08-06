6ab0c2dda62cf721704b24e54b2d5cf3c08181cc
npc_riddlah <riddlah@yandex.ru>
2023-08-06 16:11:14 +0300

    Cloudbase reboot fix

--- elements/cloudbase-init/clean/99-sysprep.ps1
+++ elements/cloudbase-init/clean/99-sysprep.ps1
@@ -1,16 +1,5 @@
-echo "Pending reboot:"
-Test-PendingReboot -Detailed
-if ( (Test-PendingReboot).IsRebootPending )
-{
-        Restart-Computer
-        Stop-Process -Name "cmd" -Force
-        Start-Sleep -Second 120.0
-}
-else
-{
-        Start-Sleep -Seconds 60.0
-	cmd.exe /c net user Administrator /active:no
-	cmd.exe /c net user Администратор /active:no
-	cmd.exe /c c:\windows\system32\sysprep\sysprep.exe /oobe /generalize /quit /unattend:c:\progra~1\cloudb~1\cloudb~1\conf\Unattend.xml
-	exit
-}
+Start-Sleep -Seconds 60.0
+cmd.exe /c net user Administrator /active:no
+cmd.exe /c net user Администратор /active:no
+cmd.exe /c c:\windows\system32\sysprep\sysprep.exe /oobe /generalize /quit /unattend:c:\progra~1\cloudb~1\cloudb~1\conf\Unattend.xml
+exit
--- elements/cloudbase-init/install/10-cloudbase-install.cmd
+++ elements/cloudbase-init/install/10-cloudbase-install.cmd
@@ -1,2 +1,13 @@
-msiexec /i "C:\tools\CloudbaseInitSetup_1_1_4_x64.msi" /qb /l* "C:/log.txt"
-exit
+echo "Pending reboot:"
+Test-PendingReboot -Detailed
+if ( (Test-PendingReboot).IsRebootPending )
+{
+        Restart-Computer
+        Stop-Process -Name "cmd" -Force
+        Start-Sleep -Second 120.0
+}
+else
+{
+	msiexec /i "C:\tools\CloudbaseInitSetup_1_1_4_x64.msi" /qb /l* "C:/log.txt"
+	exit
+}
