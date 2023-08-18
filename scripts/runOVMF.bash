30c616456360564f2804f77561c5317d6a209e90
npc_riddlah <riddlah@yandex.ru>
2023-08-18 15:43:57 +0300

    Now qemu start with -cpu host flag

--- scripts/runOVMF.bash
+++ scripts/runOVMF.bash
@@ -5,9 +5,9 @@
 
 if [ "$3" = true ]; then
 #	qemu-system-x86_64 -accel kvm -m 2048 -boot c -hda $1 -vga virtio -serial file:$1.log -spice port=$2,addr=0.0.0.0,disable-ticketing=on -bios /usr/share/qemu/OVMF.fd -smp cores=4 -cpu Skylake-Client-v3
-	qemu-system-x86_64 -enable-kvm -machine q35,accel=kvm -m 8128 -boot c -hda $1 -vga virtio -serial stdio -spice port=$2,addr=0.0.0.0,disable-ticketing=on -bios /usr/share/qemu/OVMF.fd -smp cores=8
+	qemu-system-x86_64 -enable-kvm -machine q35,accel=kvm -m 8128 -boot c -hda $1 -vga virtio -serial stdio -spice port=$2,addr=0.0.0.0,disable-ticketing=on -bios /usr/share/qemu/OVMF.fd -smp cores=8 -cpu host
 else
-	qemu-system-x86_64 -enable-kvm -machine q35,accel=kvm -m 8128 -boot c -hda $1 -vga virtio -serial file:$1.log -bios /usr/share/qemu/OVMF.fd -smp cores=8 
+	qemu-system-x86_64 -enable-kvm -machine q35,accel=kvm -m 8128 -boot c -hda $1 -vga virtio -serial file:$1.log -bios /usr/share/qemu/OVMF.fd -smp cores=8 -cpu host
 fi
 exit 0
 
