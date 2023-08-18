3d87c7cb4684de55a6bbb4f1df3ed6c395a9f041
npc_riddlah <riddlah@yandex.ru>
2023-08-18 15:44:39 +0300

    Initial corona for 3dsmax element

---
+++ elements/3d/corona_3dsmax/clean/10-clean-corona.ps1
@@ -0,0 +1,1 @@
+del /F C:\tools\chaos-corona-10-3dsmax-hotfix1.exe
---
+++ elements/3d/corona_3dsmax/install/10-install-corona.ps1
@@ -0,0 +1,3 @@
+C:\tools\chaos-corona-10-3dsmax-hotfix1.exe --acceptEulaAndAutoInstall
+del /F C:\tools\chaos-corona-10-3dsmax-hotfix1.exe
+exit
